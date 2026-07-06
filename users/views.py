8901a622509241e3e090db253aa84062aca23238
Alecdev-10 <kounouewaa@gmail.com>
2026-07-06 17:37:20 +0100

    Implement JWT authentication API

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import User
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = RegisterSerializer(request.user)
+        return Response(serializer.data)
